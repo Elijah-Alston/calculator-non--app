abcd1f1eaac14fa05043a74e47657a71e4774af8
flameking1000 <elijahalstonjr@gmail.com>
2024-05-28 14:14:08 -0400

    added some js function

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+function myfun(){
+
+    document.getElementById("demo").style.color = "red";
+
+
+}
+
+
+
+function nofun() {
+    document.getElementById("demo").style.color = "black";
+}
+
+
+function MB(){
+
+Document.getElementById("MB").style.color = "yellow"
+
+
+}
